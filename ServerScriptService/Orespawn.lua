cd1396a42338cf156c5b8fe88a04c522e8f76c1f
ppket <ppkey7@gmail.com>
2025-06-12 15:31:40 +0300

    Updated ServerScriptService/Orespawn.lua

---
+++ ServerScriptService/Orespawn.lua
@@ -0,0 +1,15 @@
+-- @ScriptType: Script
+local RS = game:GetService("ReplicatedStorage")
+local ore = RS.Ores
+
+function spawnOre()
+	
+
+end
+spawnOre()
+
+if game.Workspace:FindFirstChild("UrOre") then
+	print("Uranium Ore has been spawned!")
+else
+	print("No Uranium Ore spawned yet...")
+end
